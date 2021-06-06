75117c04095c4e53a4ab9b0e517b5028a2882544
KatieMReynolds <38407902+KatieMReynolds@users.noreply.github.com>
2021-06-05 20:12:13 -0700

    Branch3 Menu commit

--- Assets/Scripts/PuzzleSelect.cs
+++ Assets/Scripts/PuzzleSelect.cs
@@ -9,14 +9,11 @@ public class PuzzleSelect : MonoBehaviour
     //StartPanel is not a panel, but the image background renamed
     public void SetPuzzlesPhoto(Image Photo)
     {
-        //TO DO: GET THE SELECTED SCENE AND SEND THAT DATA TO THE NEXT SCENE
-
-        //assigns parts of the puzzle image to the corresponding pieces
         for (int i = 0; i < 36; i++)
         {
             GameObject.Find("Piece (" + i + ")").transform.Find("Puzzle").GetComponent<SpriteRenderer>().sprite = Photo.sprite;
         }
         StartPanel.SetActive(false);
-        //SceneManager.LoadScene(2);//loads next scene
+        
     }
 }
